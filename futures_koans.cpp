7c367e90d296b366ce344eb2784bc4fc79c2032a
Ankush Agarwal <ankushagarwal11@gmail.com>
2018-10-28 00:30:40 -0700

    Update

--- futures_koans.cpp
+++ futures_koans.cpp
@@ -1,39 +1,34 @@
-#include <iostream>
-#include <gtest/gtest.h>
 #include <folly/futures/Future.h>
+#include <folly/executors/ThreadedExecutor.h>
+#include <glog/logging.h>
 
 using namespace std;
 using namespace folly;
 
-using std::string;
-class MemcacheClient {
-public:
-    struct GetReply {
-        enum class Result {
-            FOUND,
-            NOT_FOUND,
-            SERVER_ERROR,
-        };
-
-        Result result;
-        // The value when result is FOUND,
-        // The error message when result is SERVER_ERROR or CLIENT_ERROR
-        // undefined otherwise
-        string value;
-    };
-
-    GetReply get(string key) {
-        return GetReply{.result = GetReply::Result::FOUND, .value = "some value"};
-    }
-
-    SemiFuture<GetReply> future_get(string key);
-};
-
-TEST(Futures, Basics) {
-
-}
-
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1;
+    FLAGS_minloglevel=0;
+
+    folly::ThreadedExecutor executor;
+    auto pair = makePromiseContract<int>();
+    Promise<int> promise = std::move(pair.first);
+    executor.add([&promise]() {
+        LOG(INFO) << "Inside executor";
+//        this_thread::sleep_for(std::chrono::milliseconds(100));
+        promise.setValue(100);
+        LOG(INFO) << "Completed executor";
+    });
+
+    Future<Unit> fu = std::move(pair.second)
+            .via(&executor)
+            .within(std::chrono::milliseconds(20))
+            .onError([](FutureTimeout &e) {
+                LOG(INFO) << "FutureTimeout: " << e.what();
+                return -1;
+            }).thenValue([](int i){
+                LOG(INFO) << "Got " << i;
+            });
+    this_thread::sleep_for(std::chrono::milliseconds(1000));
+    LOG(ERROR) << "Exiting main thread";
 }
