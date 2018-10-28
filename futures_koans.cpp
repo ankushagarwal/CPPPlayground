d5f4c720819937663fa923e2c3be408eafa292a6
Ankush Agarwal <ankushagarwal11@gmail.com>
2018-10-28 09:18:27 -0700

    Update

--- futures_koans.cpp
+++ futures_koans.cpp
@@ -5,30 +5,43 @@
 using namespace std;
 using namespace folly;
 
+class ManualExecutor : public Executor {
+private:
+    int i_;
+
+public:
+    explicit ManualExecutor(int i) : i_(i) {}
+    void add(Func f) override {
+        LOG(INFO) << i_ << " Pushed work onto executor";
+        LOG(INFO) << i_ << " Starting a new thread for executing the work";
+        thread th(move(f));
+        th.detach();
+    }
+};
+
+void foo(string msg, int ms = 100) {
+    LOG(INFO) << msg << " started";
+    this_thread::sleep_for(chrono::milliseconds(ms));
+    LOG(INFO) << msg << " completed";
+}
+
 int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
     FLAGS_logtostderr = 1;
-    FLAGS_minloglevel=0;
+    FLAGS_minloglevel = 0;
+    LOG(INFO) << "Main thread";
 
-    folly::ThreadedExecutor executor;
-    auto pair = makePromiseContract<int>();
-    Promise<int> promise = std::move(pair.first);
-    executor.add([&promise]() {
-        LOG(INFO) << "Inside executor";
-//        this_thread::sleep_for(std::chrono::milliseconds(100));
-        promise.setValue(100);
-        LOG(INFO) << "Completed executor";
+    shared_ptr<ManualExecutor> e1 = make_shared<ManualExecutor>(1);
+    shared_ptr<ManualExecutor> e2 = make_shared<ManualExecutor>(2);
+    auto x = e1.get();
+    via(e1.get(), []() {
+        foo("first item", 1000);
+    }).then([](){
+        foo("second item", 1000);
+    }).via(e2.get()).then([](){
+        foo("third item", 1000);
     });
-
-    Future<Unit> fu = std::move(pair.second)
-            .via(&executor)
-            .within(std::chrono::milliseconds(20))
-            .onError([](FutureTimeout &e) {
-                LOG(INFO) << "FutureTimeout: " << e.what();
-                return -1;
-            }).thenValue([](int i){
-                LOG(INFO) << "Got " << i;
-            });
-    this_thread::sleep_for(std::chrono::milliseconds(1000));
-    LOG(ERROR) << "Exiting main thread";
+    LOG(INFO) << "Sleeping main thread";
+    this_thread::sleep_for(chrono::milliseconds(10000));
+    LOG(INFO) << "Exiting main thread";
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
 #include <folly/futures/Future.h>
+#include <folly/executors/ThreadedExecutor.h>
+#include <glog/logging.h>
 
-using namespace folly;
 using namespace std;
+using namespace folly;
 
-int main() {
-    Optional<int> op;
-    return 0;
+
+int main(int argc, char **argv) {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1;
+    FLAGS_minloglevel = 0;
+    LOG(INFO) << "Main thread";
+
+
+    LOG(INFO) << "Exiting main thread";
 }
